4021c23882a00ec8e2744847e73e87040ef05e0e
Vimal Mudalagi <vimalmudalagi123@gmail.com>
2024-06-09 04:06:53 +0530

    Added correct firebase config

--- src/helpers/Firebase.js
+++ src/helpers/Firebase.js
@@ -1,21 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+//import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBk2uCyKAKkkXenT5tcRGkpmcL11PBcFIs",
-  authDomain: "moodcheckin-f072b.firebaseapp.com",
-  projectId: "moodcheckin-f072b",
-  storageBucket: "moodcheckin-f072b.appspot.com",
-  messagingSenderId: "832788979567",
-  appId: "1:832788979567:web:7e7867fc74994d023fd29a",
-  measurementId: "G-MNLYR68JB3",
+  apiKey: "AIzaSyA0fkEdrvzziA5RvVCSI0qDfUrl_s_HGIE",
+  authDomain: "moodcheckin-cfa29.firebaseapp.com",
+  projectId: "moodcheckin-cfa29",
+  storageBucket: "moodcheckin-cfa29.appspot.com",
+  messagingSenderId: "1002046118447",
+  appId: "1:1002046118447:web:570c297dbee794f22384da"
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-export const analytics = getAnalytics(app);
+//export const analytics = getAnalytics(app);
