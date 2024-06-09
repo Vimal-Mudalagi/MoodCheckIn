1478960f66d522e292a2c53cd1b6569eeab4336e
mukulpadwal <mukulpadwal.me@gmail.com>
2024-06-09 18:48:17 +0530

    FIX : register-page-ui

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 import { createUserWithEmailAndPassword } from "firebase/auth";
-import { useNavigate } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { useAuth } from "../Context/AuthContext";
-import { auth } from "../helpers/Firebase"; 
+import { auth } from "../helpers/Firebase";
 
 const RegisterPage = () => {
   const [email, setEmail] = useState("");
@@ -32,60 +32,76 @@ const RegisterPage = () => {
   };
 
   return (
-    <div className="border w-full min-h-screen flex flex-col items-center justify-center gap-y-6">
-      <h1 className="text-2xl text-center font-bold my-6">
-        MoodCheckIn | Sign Up
-      </h1>
-      <form
-        onSubmit={handleSubmit}
-        className="w-1/4 flex flex-col justify-center items-center gap-y-6"
-      >
-        <div className="w-full flex flex-row justify-between items-center gap-x-4">
-          <label htmlFor="email" className="text-lg font-semibold text-gray-900">
-            Your email
-          </label>
-          <input
-            onChange={(e) => setEmail(e.target.value)}
-            value={email}
-            type="email"
-            id="email"
-            className="p-3 border rounded-lg"
-            placeholder="name@acerite.com"
-            autoComplete="username"
-            required
-          />
-        </div>
+    <div className="w-full min-h-screen flex justify-center items-center">
+      <div className="m-4 p-4 w-full border border-black rounded-lg flex flex-col items-center justify-center gap-y-6">
+        <h1 className="text-2xl text-center font-bold my-6">
+          Mood<span className="text-[#a486f4]">CheckIn</span> | Sign Up
+        </h1>
 
-        <div className="w-full flex flex-row justify-between items-center gap-x-4">
-          <label htmlFor="password" className="text-lg font-semibold text-gray-900">
-            Your password
-          </label>
-          <input
-            onChange={(e) => setPassword(e.target.value)}
-            value={password}
-            type="password"
-            id="Password"
-            className="p-3 border rounded-lg"
-            placeholder="Your Password"
-            autoComplete="current-password"
-            required
-          />
-        </div>
+        <form
+          onSubmit={handleSubmit}
+          className="w-full flex flex-col justify-center items-center gap-y-6 p-2"
+        >
+          <div className="w-full flex flex-row justify-between items-center gap-x-4">
+            <label
+              htmlFor="email"
+              className="text-lg font-semibold text-gray-900"
+            >
+              Your email
+            </label>
+            <input
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              type="email"
+              id="email"
+              className="p-3 border rounded-lg"
+              placeholder="name@acerite.com"
+              autoComplete="username"
+              required
+            />
+          </div>
 
-        <div className="w-full flex flex-col items-center justify-center gap-y-4">
-          <button className="w-3/4 h-10 border rounded-lg text-center dark:bg-[#a48bef] dark:hover:bg-[#9373f2]">
-            Create Account
-          </button>
-          <span>----- OR -----</span>
-          <button
-            onClick={() => {}}
-            className="w-3/4 h-10 border rounded-lg text-center dark:bg-[#f84222] dark:hover:bg-[#cd2424]"
-            disabled
-          >
-            SignUp with Google
-          </button>
-        </div>
-      </form>
+          <div className="w-full flex flex-row justify-between items-center gap-x-4">
+            <label
+              htmlFor="password"
+              className="text-lg font-semibold text-gray-900"
+            >
+              Your password
+            </label>
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              type="password"
+              id="Password"
+              className="p-3 border rounded-lg"
+              placeholder="Your Password"
+              autoComplete="current-password"
+              required
+            />
+          </div>
+
+          <div className="w-full flex flex-col items-center justify-center gap-y-4">
+            <button className="w-3/4 h-10 border rounded-lg text-center dark:bg-[#a48bef] dark:hover:bg-[#9373f2]">
+              Create Account
+            </button>
+            <span>----- OR -----</span>
+            <button
+              onClick={() => {}}
+              className="w-3/4 h-10 border rounded-lg text-center dark:bg-[#f84222] dark:hover:bg-[#cd2424]"
+              disabled
+            >
+              SignUp with Google
+            </button>
+          </div>
+        </form>
+
+        <h1 className="text-lg text-center font-bold my-4">
+          Already have an account?{" "}
+          <NavLink to={"/login"}>
+            <span className="text-[#a486f4]">Login</span>
+          </NavLink>
+        </h1>
+      </div>
     </div>
   );
 };
